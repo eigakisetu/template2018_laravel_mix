7a660b9a349ecd657a7dcb7806e95eda5d198726
佐竹映季 <satake@tam-tam.co.jp>
2018-02-02 16:30:27 +0900

    webpack browserSync追加

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,9 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/assets/js')
-	.sass('resources/assets/scss/style.scss', 'public/assets/css');
+	.sass('resources/assets/scss/style.scss', 'public/assets/css')
+	.browserSync({
+		files: 'public/**/*',
+		server: 'public',
+		proxy: false
+	});
